c782e1682783237b6b1880d240eb37ebe318f8a4
Robert Bunch <rob@digitalcrafts.com>
2016-03-28 14:01:54 -0400

    Update to use cities object and increment over it

--- scripts.js
+++ scripts.js
@@ -2,15 +2,24 @@ var myApp = angular.module('myApp', []);
 // console.log(myApp);
 myApp.controller('myController', function($scope){
 
-	// $scope.first = "Rob";
-	// $scope.last = "Bunch";
-
-	$scope.dcClass = ['Tristan','Josh','Bogden','Lazlo','Keith','Will','Curtis','Joe','Kochan','Patrick','Jonathan','Jeremy'];
+	$scope.cities = [
+		{
+			name: "Atlanta",
+			population: 500000
+		},
+		{
+			name: "Houston",
+			population: 2200000
+		},
+		{
+			name: "Portland",
+			population: 610000
+		}
+	]
 
 	$scope.addStudent = function(){
 		$scope.dcClass.push($scope.newStudent);
 		$scope.newStudent = '';
-		console.log($index);
 	}
 
 	$scope.removeStudent = function(i){
