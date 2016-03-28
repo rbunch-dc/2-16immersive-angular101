17bb3de16cd0671f04dbecb2e70e30becc725437
Robert Bunch <rob@digitalcrafts.com>
2016-03-28 10:52:38 -0400

    Angular exercises - part1

---
+++ scripts.js
@@ -0,0 +1,18 @@
+
+// --------The plan vanilla JS way--------- //
+// var myInput = document.getElementById('myInput');
+
+// myInput.addEventListener('keyup', function(){
+// 	// console.log('User hit a key!');
+// 	var userText = myInput.value;
+// 	document.getElementById('inputText').innerHTML = userText
+
+// })
+
+
+// ------------ The jQuery way! ------------ //
+// $('#myInput').keyup(function(){
+// 	$('#inputText').html($('#myInput').val());
+// })
+
+
