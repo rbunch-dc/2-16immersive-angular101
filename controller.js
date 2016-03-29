2552a2c7e4061054bd38f1736e0ce15aa6bcb989
Robert Bunch <rob@digitalcrafts.com>
2016-03-29 10:48:13 -0400

    Extract data

--- controller.js
+++ controller.js
@@ -1,4 +1,35 @@
 var myApp = angular.module('myApp',[]);
 myApp.controller('myController', function($scope){
 
+	console.log(countries);
+
+	$scope.countries = countries;
+
+	console.log($scope.countries);
+
+
+	$scope.addCountry = function(){
+		$scope.tempID = 'test div';
+		$scope.message = 'Your coutnry was added!!';
+
+		// $scope.countryName = 'I changed you!!';
+		$scope.countries.push(
+			{
+				name: $scope.countryName,
+				population: $scope.population,
+				president: $scope.president,
+				src: $scope.flag
+			}
+		)
+	}
+
+
+	$scope.removeCountry = function(i){
+		$scope.message = $scope.countries[i].name + ' was removed!!';
+		$scope.countries.splice(i,1);
+		$scope.tempID = 'test div';
+
+	}
+
+
 });
---
+++ countries.js
@@ -0,0 +1,62 @@
+	var countries = [
+		{
+	        name: 'America',
+	        population: '318,900,000',
+	        president: 'Obama',
+	        src: 'http://www.flags-by-swi.com/F25-bandana.JPEG'
+	    },
+	    {
+	        name: 'Canada',
+	        population: '35,160,000',
+	        president: 'Trudeau',
+	        src: 'http://www.flags-by-swi.com/F25-bandana.JPEG'
+	    },
+	    {
+	        name: 'Brazil',
+	        population: '200,400,000',
+	        president: 'Rousseff',
+	        src: 'http://www.flags-by-swi.com/F25-bandana.JPEG'
+	    },
+	    {
+	        name: 'Japan',
+	        population: '127,300,000',
+	        president: 'Abe',
+	        src: 'http://www.flags-by-swi.com/F25-bandana.JPEG'
+	    },
+	        {
+	        name: 'China',
+	        population: '1,357,300,000',
+	        president: 'Jinping',
+	        src: 'http://www.flags-by-swi.com/F25-bandana.JPEG'
+	    },
+	        {
+	        name: 'Korea',
+	        population: '50,220,000',
+	        president: 'Geun-hye',
+	        src: 'http://www.flags-by-swi.com/F25-bandana.JPEG'
+	    },
+	        {
+	        name: 'Russia',
+	        population: '143,500,000',
+	        president: 'Putin',
+	        src: 'http://www.flags-by-swi.com/F25-bandana.JPEG'
+	    },
+	        {
+	        name: 'Australia',
+	        population: '23,130,000',
+	        president: 'Abbott',
+	        src: 'http://www.flags-by-swi.com/F25-bandana.JPEG'
+	    },
+	        {
+	        name: 'Ireland',
+	        population: '4,595,000',
+	        president: 'Higgins',
+	        src: 'http://www.flags-by-swi.com/F25-bandana.JPEG'
+	    },
+	        {
+	        name: 'Germany',
+	        population: '80,620,000',
+	        president: 'Gauck',
+	        src: 'http://www.flags-by-swi.com/F25-bandana.JPEG'
+	    }
+	];
